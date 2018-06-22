87e0f0f7fb66dd2ad5c274741957a11343791ede
Abhinav K Subramanian <abhinavksubramanian@Abhinavs-MacBook-Air.local>
2018-06-22 16:37:46 +0530

    Added guest user for current user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,9 @@
 class ApplicationController < ActionController::Base
  
   include DeviseWhitelist
+  
+  include SetSource
 
-  before_action :set_source
-
-  def set_source
-    session[:source] = params[:q] if params[:q]
-  end 
+  include CurrentUserConcern
 
 end
